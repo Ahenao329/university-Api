181a24c9a12cd2b6f4907b22976509fb19a125a5
Ahemao <andersonhenao329@gmail.com>
2024-12-28 15:43:53 -0500

    m

---
+++ UniversityApi/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApi.Models.DataModels;
+
+namespace UniversityApi.DataAccess
+{
+    public class UniversityDbContext: DbContext
+    {
+        public UniversityDbContext(DbContextOptions<UniversityDbContext> options) : base(options)
+        {
+            
+        }
+        // TODO: Add DbSets (Tables od our Data base) 
+        public DbSet<User>? Users { get; set; }
+    }
+}
---
+++ UniversityApi/Models/DataModels/Curso.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApi.Models.DataModels
+{
+    public class Curso
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [Required, StringLength(280)]
+        public string ShortDescription { get; set; } = string.Empty;
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        public string Objetivos { get; set; } = string.Empty;
+        public string Requisitos { get; set; } = string.Empty;
+
+        public Level Level { get; set; } = Level.Basic;
+    }
+
+    public enum Level
+    {
+        Basic,
+        Medium,
+        Advanced,
+        Expert
+    }
+
+}
